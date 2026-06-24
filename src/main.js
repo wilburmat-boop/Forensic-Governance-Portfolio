6a508109ec81d58be3004d6498a18545420064a6
EthicHawks.co.za <info@ethichawks.co.za>
2026-06-25 00:52:56 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.querySelector('#root').innerHTML = `
+  <div style="font-family: sans-serif; padding: 2rem; text-align: center;">
+    <h1>Ethical Forensic Governance</h1>
+    <p>System Operational: Integrity Verified</p>
+  </div>
+`;
